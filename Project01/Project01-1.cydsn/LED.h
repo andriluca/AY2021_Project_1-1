74b53d2fc0578c2e905befa09f570b95bab2a131
Luca Andriotto <luca.andriotto@mail.polimi.it>
2020-12-24 18:36:12 +0100

    Aggiunte funzioni per interfacciamento con LED.

---
+++ Project01/Project01-1.cydsn/LED.h
@@ -0,0 +1,23 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+
+#ifndef __LED_H
+    #define __LED_H
+    #include "project.h"
+    #include "define.h"
+    
+    void LED_BlinkSlow();
+    void LED_BlinkFast();
+    
+#endif
+
+/* [] END OF FILE */
