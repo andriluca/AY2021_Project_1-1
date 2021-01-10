e460254ba279254e7959fb14cf188e9d03d1b973
Luca Andriotto <luca.andriotto@mail.polimi.it>
2021-01-10 17:45:37 +0100

    Shell script ultimati. Pronto al merge con main.

--- INSTALL.sh
+++ INSTALL.sh
@@ -6,18 +6,19 @@ pipinstall(){
 	# Checking the installation of pip
 	! [ -x /usr/bin/pip ] || ! [ -x /usr/bin/pip3 ] && \
 	# User OS selection
-	echo -e "\nRequested SU privileges to install python-pip\nSelect the number of the operative system currently in use.
+	echo "\nRequested SU privileges to install python-pip\nSelect the number of the operative system currently in use.
 		\n(1) Arch based\n(2) Debian based\n(3) RHEL/Fedora\n(4) Gentoo based\n(5) macOS" && \
 	read input
 	case $input in
 		1)echo "Using pacman..."; sudo pacman -Syu; sudo pacman -S python-pip; checkmembership;;
 		2)echo "Using apt..."; sudo apt update; sudo apt install python3-pip; checkmembership;;
 		3)echo "Using dnf..."; sudo dnf install python-pip; checkmembership;;
-		4)echo "Using emerge..."; emerge --ask dev-python/pip checkmembership;;
+		4)echo "Using emerge..."; emerge --ask dev-python/pip; checkmembership;;
 		5) curl	-o get-pip.py https://bootstrap.pypa.io/get-pip.py;
 			chmod +x get-pip.py
 			python3 get-pip.py
 			rm get-pip.py;;
+			# The membership to uucp is not required for Mac Users.
 		*)echo "Skipping the python-pip installation!";;
 	esac
 	# If no virtual environment is present, manage a new install.
